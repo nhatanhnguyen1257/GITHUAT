672b5ae8ee483419d6f8b246cdc3658842f2f20a
nhat <nhatanh.nguyen.2795@gmail.com>
2018-07-08 13:30:12 +0700

    bai toan lam tron 400000 so sau dau phay cua phep tinh thap phan

--- VuTru.java
+++ VuTru.java
@@ -1,17 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 
  */
 
 /**
  * @author: Nhatanh
- * create date: Jul 7, 2018
+ * create date: Jul 8, 2018
  */
 public class VuTru {
-
-  private String number = "";
-  
-  // chia nhỏ các khoảng 10^ 4
-  private final int TEMP = 10000;
   
   // khoang lam tron sau dau phay
   private final int SIZE = 400000;
@@ -20,79 +18,77 @@ public class VuTru {
   private int mu = 0;
   
   
+  Map<Integer, String> map = new HashMap<Integer, String>();
+  
   /**
    * kiểm tra xem thuộc loại số chia hết, phân số hưu hạn hay vô hạn
    * */
-  private String math(int tuSO, int thuongSo) {
+  private String math(long tuSO, long mauSo) {
+
+    int index = 1;
     
-    if (tuSO % thuongSo == 0) {
-      return ""+ (double)(tuSO/thuongSo);
-    }else{
+    // do dai cua phan sau dau phay
+    int lenghtString = 100;
+    
+    // chua phan du cua phep toan
+    long phanDu = tuSO % mauSo;
+    
+    // tinh phan nguyen cua phan so
+    long phanNguyen = tuSO / mauSo;
+    
+    String number = ""+ phanNguyen ;
+
+    String tempNumber ="";
+    
+    if (phanDu != 0) {
+      number += ".";
       
-      if ((tuSO * Math.pow(10, 1 +
-                    Math.abs( String.valueOf(tuSO).toString().length()
-                        - String.valueOf(thuongSo).toString().length())))
-                          % thuongSo == 0) {
-
-        return ""+ (double)tuSO  / thuongSo;
+      while(true) {
+        mu ++;
+        lenghtString --;
+        tuSO = phanDu * 10; 
+        phanNguyen = tuSO / mauSo;
         
-      }else {
-        
-        if ( tuSO < thuongSo) {
-          tuSO *= Math.pow(10,
-              Math.abs( String.valueOf(tuSO).length() - String.valueOf(thuongSo).length()));
-          mu += (String.valueOf(TEMP).length() - 1);
+        if (phanNguyen == 0) {
+          tuSO = phanDu * 10;
+          tempNumber += 0;
+        }else {
+          tempNumber += phanNguyen;
         }
         
-        number += ""+tuSO / thuongSo +".";
-        tuSO = tuSO % thuongSo;
-
-        for( int i = 0; i <= SIZE/TEMP; i++) {
+        phanDu = tuSO % mauSo;
           
-          number += ""+ tuSO * TEMP /thuongSo;
-          tuSO = tuSO % thuongSo;
-          mu += (String.valueOf(TEMP).length() - 1);
+        if (lenghtString == 0) {
+          map.put(index, tempNumber);
+          tempNumber = "";
+          lenghtString = 100;
+          index ++;
         }
-        return number ;
+        
+        if (phanDu == 0)
+          break;
+        
+         if (mu == SIZE)
+           break;
       }
-    }
+      return number;    
+    }else 
+       return number;
+    
   }
   
-  /**
-   * nếu là sô nguyên thì vị trí thứ 2 từ cuối chuỗi sẽ là đấu .
-   * nếu không phai thi là dâu . thi là sô hưu tử vô hạn khoặc hưu han.
-   * số mũ sẽ có 2 khoảng 164 và 170
-   * 
-   * với 164 thì làm tròn tới số thứ 5 từ cuối.
-   * với 168 thì lam tròn tới kí tự thứ 9 từ cuối cùng 
-   * */
-  private String chuoi(String string) {
-    
-    if (string.charAt(string.length() - 2) == '.') {
-      return string.substring(0, string.length() - 2); 
-    }else {
-      StringBuffer stringBuffer = new StringBuffer(string);
-      for (int i = stringBuffer.length() - 1;  i >= 0; i -= String.valueOf(TEMP).length() - 1  ) {
-        for (int j = 0; j < String.valueOf(TEMP).length() - 1; j ++) {
-          stringBuffer.replace(i - j - 1 , i - j - 1, ""+(int)((int)(stringBuffer.charAt(i - j - 1) - '0') 
-                                                + (int)(stringBuffer.charAt(i) - '0') + 0.5));
-        }
-        stringBuffer.substring(0, i - String.valueOf(TEMP).length() - 1 );
-        mu -= String.valueOf(TEMP).length() - 1;
-        
-        if (mu == SIZE / TEMP * (String.valueOf(TEMP).length() - 1)) {
-          break;
-        };
-      }
-      
-      return stringBuffer.toString() +"*10^-"+ mu;
+  private void inSo(Map<Integer, String> map, int tuSo, int MauSO) {
+    System.out.print(math(tuSo, MauSO));
+    for (Map.Entry<Integer, String> m : map.entrySet()) {
+      System.out.println(m.getValue());
     }
-    
   }
+
   
   public static void main(String []args) {
     VuTru v = new VuTru();
-    System.out.println(v.chuoi(v.math(1, 512)));
+    v.inSo(v.map, 1, 3);
+    
   }
   
 }
